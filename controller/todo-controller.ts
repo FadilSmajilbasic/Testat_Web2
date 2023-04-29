ac658cf0d7b634f239b9f46cbe308dd934568592
FadilSmajilbasic <smajilbaicfadil@gmail.com>
2023-04-29 20:22:43 +0200

    progress

--- controller/todo-controller.ts
+++ controller/todo-controller.ts
@@ -1,16 +1,27 @@
 class TodoController {
     newTodo = (req: any, res: any) => {
-
         const dark = req.query.toggle_style === "true";
-		
+
         if (dark) {
             req.userSettings.dark = !req.userSettings.dark;
             res.redirect(req.originalUrl.split("?")[0]);
         } else {
-            res.render("new-todo", { data: "Hello todo", dark: req.userSettings.dark, title: "New Todo" });
+            res.render("new-todo", {
+                data: "Hello todo",
+                dark: req.userSettings.dark,
+                title: "New Todo",
+                today: this.getDay(),
+            });
         }
-
     };
+
+    private getDay() {
+        const today = new Date();
+        const day = today.getDate().toString().padStart(2, "0");
+        const month = (today.getMonth() + 1).toString().padStart(2, "0");
+        const year = today.getFullYear().toString();
+        return `${year}-${month}-${day}`;
+    }
 }
 
 export const todoController = new TodoController();
